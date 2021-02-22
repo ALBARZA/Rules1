94c92237a2ce733751be83285040f71e3cf2a622
ALBARZA <57630676+ALBARZA@users.noreply.github.com>
2021-02-22 07:14:14 +0200

    Add files via upload

---
+++ Rules.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 07:01:32 2021
+
+@author: keven
+"""
+
+import streamlit as st
+
+
+st.title("Steps to avoid error while using my application")
+
+st.markdown("""
+            **First** : Whenever you see an error, please scroll with the bar to any number you like. 
+            This error is occuring because of color="..." and I could have removed it but the graph
+            would become dry.
+            **Second** : Change the year to 2015 and less to have better visuals. The data does not have a lot
+            of rows prior to 2015
+            **Third** : I will be uploading also a python file withot the color="..." just to make it easier
+            to use.
+                """)
